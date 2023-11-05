c692883d2f719df4b179a73083764ac909e13345
mahloola <mahloola08@gmail.com>
2023-11-05 01:25:40 -0500

    fixed pointer events for svg waves, darkened waves

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
               A Discord bot for claiming and trading osu! player cards.
             </p>
             <a
-              href="#"
+              href="https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D674498529489846272%26permissions%3D8%26scope%3Dbot"
               className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
               Invite Bot
@@ -55,8 +55,8 @@ export default function Home() {
       </section>
 
       {/* SVG WAVES */}
-      <svg className="bg-transparent absolute bottom-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3f3f46" fill-opacity="1" d="M0,64L48,101.3C96,139,192,213,288,234.7C384,256,480,224,576,229.3C672,235,768,277,864,298.7C960,320,1056,320,1152,314.7C1248,309,1344,299,1392,293.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-      <svg className="bg-transparent absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3f3f46" fill-opacity="1" d="M0,96L48,80C96,64,192,32,288,26.7C384,21,480,43,576,58.7C672,75,768,85,864,106.7C960,128,1056,160,1152,149.3C1248,139,1344,85,1392,58.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      <svg className="bg-transparent absolute bottom-0 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(24 24 27)" fill-opacity="1" d="M0,64L48,101.3C96,139,192,213,288,234.7C384,256,480,224,576,229.3C672,235,768,277,864,298.7C960,320,1056,320,1152,314.7C1248,309,1344,299,1392,293.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      <svg className="bg-transparent absolute pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(24 24 27)" fill-opacity="1" d="M0,96L48,80C96,64,192,32,288,26.7C384,21,480,43,576,58.7C672,75,768,85,864,106.7C960,128,1056,160,1152,149.3C1248,139,1344,85,1392,58.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 
       {/* Commands */}
       <section className="bg-white dark:bg-zinc-950 min-h-screen flex justify-center">
@@ -67,10 +67,10 @@ export default function Home() {
               width={1000}
               height={1000}
               alt="Demo Gif"
-              className="pr-4"
+              className="mr-4 rounded-xl"
             />
           </div>
-          <div className="mr-auto ml-16 place-self-center lg:col-span-7">
+          <div className="mr-auto pl-16 place-self-center lg:col-span-7">
             <h1 className={`max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white ${styles.slogan}`}>
               Commands
             </h1>
@@ -100,7 +100,7 @@ export default function Home() {
       </section>
 
       {/* SVG WAVES 2 */}
-      <svg className="bg-transparent absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3f3f46" fill-opacity="1" d="M0,32L80,26.7C160,21,320,11,480,26.7C640,43,800,85,960,90.7C1120,96,1280,64,1360,48L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+      <svg className="bg-transparent absolute pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(24 24 27)" fill-opacity="1" d="M0,32L80,26.7C160,21,320,11,480,26.7C640,43,800,85,960,90.7C1120,96,1280,64,1360,48L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
       {/* Shopping Cart Section */}
       <section className="pt-16 bg-white dark:bg-zinc-950 min-h-screen flex justify-center">
         <div className="bg-white dark:bg-zinc-950 w-full py-8">
@@ -110,7 +110,7 @@ export default function Home() {
             </h1>
             <div className="flex flex-col md:flex-row gap-4 relative">
               <div className="md:w-3/4">
-                <div className="bg-zinc-300 dark:bg-zinc-700 rounded-lg shadow-md p-6 mb-4">
+                <div className="bg-zinc-300 dark:bg-zinc-900 rounded-lg shadow-md p-6 mb-4">
                   <table className="w-full">
                     {/* <thead>
                       <tr>
@@ -165,7 +165,7 @@ export default function Home() {
                           </div>
                         </td>
 
-                        <td className="py-4">$4.99</td>
+                        <td className="py-4">$9.99</td>
                         <td className="py-4">
                           <div className="flex items-center">
                             <button className="border rounded-md py-2 px-4 mr-2">
@@ -179,7 +179,7 @@ export default function Home() {
                         </td>
                         <td className="py-4">$0</td>
                       </tr>
-                      {/* Lootbox (30 Cards) */}
+                      {/* Lootbox (100 Cards) */}
                       <tr>
                         <td className="py-4">
                           <div className="flex items-center">
@@ -194,7 +194,7 @@ export default function Home() {
                           </div>
                         </td>
 
-                        <td className="py-4">$9.99</td>
+                        <td className="py-4">$19.99</td>
                         <td className="py-4">
                           <div className="flex items-center">
                             <button className="border rounded-md py-2 px-4 mr-2">
@@ -213,7 +213,7 @@ export default function Home() {
                 </div>
               </div>
               <div className="md:w-1/4 relative">
-                <div className="bg-zinc-300 dark:bg-zinc-700 rounded-lg shadow-md p-6">
+                <div className="bg-zinc-300 dark:bg-zinc-900 rounded-lg shadow-md p-6">
                   <h2 className="text-lg font-semibold mb-4">Summary</h2>
                   <div className="flex justify-between mb-2">
                     <span>Subtotal</span>
